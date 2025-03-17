be835e308d8f6847bb48ada47205344d97d328da
Thomas Mol <144921908+pasmoi99@users.noreply.github.com>
2025-03-17 16:41:32 +0100

    Building Animation

--- Assets/Scripts/BuildingMenuAsset.cs
+++ Assets/Scripts/BuildingMenuAsset.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MovingMenuAsset : MonoBehaviour
+public class BuildingMenuAsset : MonoBehaviour
 {
     [SerializeField] private List<Sprite> _spriteList;
     [SerializeField] private float _moveSpeed;
@@ -10,10 +10,16 @@ public class MovingMenuAsset : MonoBehaviour
     [SerializeField] private float _animationSpeed;
     [SerializeField] private float _animationDelay;
 
+
+    private int _currentFrame;
+    private float _timer;
+
     private Image _imageRenderer;
     // Start is called before the first frame update
     void Start()
     {
+        _timer = 0;
+        _currentFrame = 0;
         _imageRenderer = GetComponent<Image>();
         _imageRenderer.sprite = _spriteList[0];
     }
@@ -22,14 +28,18 @@ public class MovingMenuAsset : MonoBehaviour
     void Update()
     {
         transform.position += new Vector3(-_moveSpeed, 0, 0) * Time.deltaTime;
+        _timer += Time.deltaTime;
+        Animate();
     }
 
     void Animate()
     {
-        for (int i=1; i < _spriteList.Count; i++)
+        if (_timer >= _animationDelay && _currentFrame < _spriteList.Count - 1)
         {
-            _imageRenderer.sprite = _spriteList[i];
-        }
+            _imageRenderer.sprite = _spriteList[_currentFrame];
+            _timer = 0;
+            _currentFrame = (_currentFrame+1)%_spriteList.Count;
+        }        
 
     }
 
